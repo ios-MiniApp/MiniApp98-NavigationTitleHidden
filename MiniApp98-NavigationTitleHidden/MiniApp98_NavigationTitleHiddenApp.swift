f17c3b90409ffaabd9f44de568d8f811016c83c4
Kota <maedakoutaa@gmail.com>
2022-09-19 22:54:09 +0900

    Initial Commit

---
+++ MiniApp98-NavigationTitleHidden/MiniApp98_NavigationTitleHiddenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp98_NavigationTitleHiddenApp.swift
+//  MiniApp98-NavigationTitleHidden
+//
+//  Created by 前田航汰 on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp98_NavigationTitleHiddenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
